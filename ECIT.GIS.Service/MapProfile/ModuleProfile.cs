e5b8f561bcf7cf10b6f2a4ec1cba3e64e74ff13c
liuyuqing <576479931@qq.com>
2020-04-02 17:59:24 +0800

    1.增加dto 实体映射

---
+++ ECIT.GIS.Service/MapProfile/ModuleProfile.cs
@@ -0,0 +1,30 @@
+﻿/*
+* ==============================================================================
+*
+* Filename: DepartmentProfile
+* ClrVersion: 4.0.30319.42000
+* Description: DepartmentProfile
+*
+* Version: 1.0
+* Created: 2020/1/11 14:29:04
+* Compiler: Visual Studio 2017
+*
+* Author: liuyuqing
+* Copyright: 广东满天星云信息技术有限公司
+*
+* ==============================================================================
+*/
+
+using AutoMapper;
+using ECIT.GIS.Entity;
+namespace ECIT.GIS.Service
+{
+    public class ModuleProfile : Profile
+    {
+        public ModuleProfile()
+        {
+            CreateMap<Module, moduledt>();
+            CreateMap<DepartmentDto, Department>();
+        }
+    }
+}
