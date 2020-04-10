cce9366c367fbb6b5fc0bb3f0c254ef3a77411c1
liuyuqingenglish <liuyuqingenglish@126.com>
2020-04-10 09:00:36 +0800

    23

---
+++ ECIT.GIS.WebService/Atributer/ActionFilter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http.Filters;
+namespace ECIT.GIS.WebService
+{
+    public class ActionFilter : ActionFilterAttribute
+    {
+        public override void OnActionExecuted(HttpActionExecutedContext actionExecutedContext)
+        {
+            if (actionExecutedContext.ActionContext.Response.IsSuccessStatusCode)
+            {
+                //actionExecutedContext.ActionContext.Response.Content.
+            }
+        }
+    }
+}
