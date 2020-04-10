9be290f1444e5fe52c832e87a945d0872ac41f80
liuyuqing <576479931@qq.com>
2020-04-10 18:45:57 +0800

    1.封装回复消息

---
+++ ECIT.GIS.Common/WebResult/WebResponseResult.cs
@@ -0,0 +1,50 @@
+﻿/*
+* ==============================================================================
+*
+* Filename: WebResponseResult
+* ClrVersion: 4.0.30319.42000
+* Description: WebResponseResult
+*
+* Version: 1.0
+* Created: 2020/4/10 10:54:27
+* Compiler: Visual Studio 2017
+*
+* Author: liuyuqing
+* Copyright: 广东满天星云信息技术有限公司
+*
+* ==============================================================================
+*/
+
+namespace ECIT.GIS.Common.WebResult
+{
+    public class WebResponseResult<TResult>
+    {
+        /// <summary>
+        /// 状态码
+        /// </summary>
+        public string Code;
+
+        /// <summary>
+        /// 是否成功
+        /// </summary>
+        public bool IsSuccess;
+
+        /// <summary>
+        /// 失败原因
+        /// </summary>
+        public string ErrorInfo;
+
+        /// <summary>
+        /// 返回实体
+        /// </summary>
+        public object Entity;
+
+        public WebResponseResult(string code, bool success, object entity, string error)
+        {
+            this.Code = code;
+            this.IsSuccess = success;
+            this.Entity = entity;
+            this.ErrorInfo = error;
+        }
+    }
+}
