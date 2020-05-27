58f0e346e9b60c08d0b9ceda88bed540388d1f21
liuyuqing <576479931@qq.com>
2020-05-27 16:10:27 +0800

    11

---
+++ ECIT.GIS.Common/Extension/StringExtension.cs
@@ -0,0 +1,40 @@
+﻿/*
+* ==============================================================================
+*
+* Filename: StringExtension
+* ClrVersion: 4.0.30319.42000
+* Description: StringExtension
+*
+* Version: 1.0
+* Created: 2020/5/27 11:45:18
+* Compiler: Visual Studio 2017
+*
+* Author: liuyuqing
+* Copyright: 广东满天星云信息技术有限公司
+*
+* ==============================================================================
+*/
+
+using System.Configuration;
+
+namespace ECIT.GIS.Common
+{
+    public static class StringExtension
+    {
+        public static string GetConfigData(this string configKey, string defaultData = null)
+        {
+            if (ConfigurationManager.AppSettings[configKey] != null)
+            {
+                return ConfigurationManager.AppSettings[configKey].ToString();
+            }
+            else
+            {
+                if (!string.IsNullOrEmpty(defaultData))
+                {
+                    return defaultData;
+                }
+            }
+            return string.Empty;
+        }
+    }
+}
