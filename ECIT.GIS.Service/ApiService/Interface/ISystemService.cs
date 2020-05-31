03cbf8466047e9f609ab57453de7f6855fa4b22b
liuyuqing <576479931@qq.com>
2020-05-31 21:12:55 +0800

    1.文件提交

---
+++ ECIT.GIS.Service/ApiService/Interface/ISystemService.cs
@@ -0,0 +1,18 @@
+﻿using ECIT.GIS.Entity;
+using ECIT.GIS.Protocol;
+using System;
+using System.Collections.Generic;
+
+namespace ECIT.GIS.Service
+{
+    public interface ISystemService
+    {
+        List<SystemDto> GetSystemDto(ProtocolQuerySystem query);
+
+        bool AddSystem(SystemDto dto);
+
+        bool UpdateSystem(SystemDto dto);
+
+        bool DeleteSystem(Guid sysId);
+    }
+}
