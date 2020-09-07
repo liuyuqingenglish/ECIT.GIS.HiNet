73ec9d0858eee370ca2d834f4201feb5b493a515
liuyuqing <576479931@qq.com>
2020-09-07 17:40:04 +0800

    1.添加自宿主项目

---
+++ ECIT.GIS.ServiceConsoleHost/Program.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Owin.Hosting;
+using System;
+using ECIT.GIS.WebService;
+namespace ConsoleApp1
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            string baseAddress = "http://localhost:9003/";
+            Microsoft.Owin.Hosting.WebApp.Start<Startup>(url: baseAddress);
+            Console.WriteLine("程序已启动,按任意键退出");
+            Console.ReadLine();
+        }
+    }
+}
