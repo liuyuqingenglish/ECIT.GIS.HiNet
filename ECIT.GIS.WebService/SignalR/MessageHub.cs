cd4d61d502cf70d63423b661fe0edcd9b17cdd4d
liuyuqing <576479931@qq.com>
2020-09-12 14:54:58 +0800

    1.增加双向通信接口

--- ECIT.GIS.WebService/SignalR/MessageHub.cs
+++ ECIT.GIS.WebService/SignalR/MessageHub.cs
@@ -1,27 +1,30 @@
 ﻿using Microsoft.AspNet.SignalR;
-using System.Collections.Concurrent;
-using System.Threading.Tasks;
-using System;
 using Microsoft.AspNet.SignalR.Hubs;
+using System;
+using System.Threading.Tasks;
+
 namespace ECIT.GIS.WebService
 {
     public class MessageHub : Hub
     {
         /// <summary>
-        /// 客户端对象
+        /// web组
         /// </summary>
-        public ConcurrentDictionary<string, IRequest> client = new ConcurrentDictionary<string, IRequest>();
+        private const string WEB_GROUP = "web_group";
+
+        /// <summary>
+        /// 移动端组
+        /// </summary>
+        private const string MOBILE_GROUP = "mobile_group";
 
         /// <summary>
         /// 连接上
         /// </summary>
         /// <returns></returns>
+        [HubMethodName("OnConnected")]
         public override Task OnConnected()
         {
-            if (!client.ContainsKey(this.Context.ConnectionId))
-            {
-                client.TryAdd(this.Context.ConnectionId, this.Context.Request);
-            }
+            Clients.All.LoginSucess($"{this.Context.ConnectionId}用户登录成功");
             return base.OnConnected();
         }
 
@@ -32,11 +35,7 @@ namespace ECIT.GIS.WebService
         /// <returns></returns>
         public override Task OnDisconnected(bool stopCalled)
         {
-            if (client.ContainsKey(this.Context.ConnectionId))
-            {
-                IRequest request = null;
-                client.TryRemove(this.Context.ConnectionId, out request);
-            }
+            Clients.AllExcept(Context.ConnectionId).LoginOut($"{this.Context.ConnectionId}用户注销登录");
             return base.OnDisconnected(stopCalled);
         }
 
@@ -46,17 +45,15 @@ namespace ECIT.GIS.WebService
         /// <returns></returns>
         public override Task OnReconnected()
         {
-            if (client.ContainsKey(this.Context.ConnectionId))
-            {
-                client[this.Context.ConnectionId] = this.Context.Request;
-            }
             return base.OnReconnected();
         }
 
         [HubMethodName(nameof(SendMessage))]
         public void SendMessage(string message)
         {
-            Console.WriteLine(message);
+            string id = this.Context.ConnectionId;
+            Clients.All.RecevieMessage(id, message);
         }
+
     }
 }
